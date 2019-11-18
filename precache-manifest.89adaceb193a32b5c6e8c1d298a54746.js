1ba804df70a829a8763b231e6c27b5d0fc251d33
malsevmax <malsevmax92@gmail.com>
2019-11-19 02:11:16 +0300

    Updates

---
+++ precache-manifest.89adaceb193a32b5c6e8c1d298a54746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2eb478238c6fe182fa8bffd17889a2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9997f95c19826da80cf8",
+    "url": "/monsters-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "9997f95c19826da80cf8",
+    "url": "/monsters-rolodex/static/js/main.9101dbe0.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
